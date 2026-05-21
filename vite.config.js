93044f54db075105b6eecbaaec2d4e049f7d4514
777natalie <casandra.n.roman-1@ou.edu>
2026-05-21 12:51:52 -0500

    added path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // This matches your repository name exactly [2]
   plugins: [react()],
 })
